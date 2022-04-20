6c2e02b5f122d56533c22c3ada4931d5e08d2754
asus <abhinaygarg8@hotmail.com>
2022-04-20 18:27:17 +0530

    Adding backend

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+// const hbs = require('hbs');
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+// app.set('view engine', 'hbs');
+
+// app.use(express.static("./css/styles.css"));
+// app.use(express.static("/images"));
+// app.use(express.static("/js"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,'/index.html'))
+    // res.send("Welcome to the app")
+});
+
+app.listen(port, () =>
+      console.log(`Server Running on Port: http://localhost:${port}`)
+)
